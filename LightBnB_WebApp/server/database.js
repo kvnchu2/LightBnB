f770e662fd8405d39183f28af7acdc27cdbf67fe
Kevin Chu <kvnchu2@gmail.com>
2021-01-08 03:17:19 +0000

    add insert into query for create listing

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -25,7 +25,7 @@ const getUserWithEmail = function(email) {
     return res.rows[0];
   })
   .catch(err => {
-    console.log('insidecatch')
+    console.log(err);
   });
 }
 exports.getUserWithEmail = getUserWithEmail;
@@ -44,7 +44,7 @@ const getUserWithId = function(id) {
     return res.rows[0];
   })
   .catch(err => {
-    console.log('insidecatch')
+    console.log(err);
   });
 }
 exports.getUserWithId = getUserWithId;
@@ -64,7 +64,7 @@ const addUser =  function(user) {
     return res.rows[0]; 
   })
   .catch(err => {
-    console.log('insidecatch')
+    console.log(err);
   });
 }
 exports.addUser = addUser;
@@ -187,9 +187,15 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool.query(`
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) returning *;
+  `, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+  .then(res => {
+    return res.rows[0]; 
+  })
+  .catch(err => {
+    console.log(err);
+  });
 }
 exports.addProperty = addProperty;
